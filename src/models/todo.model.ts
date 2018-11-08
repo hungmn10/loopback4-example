105bae3f3a53bbf8bf79f378fa1200a51aa6c3bd
Nguyen Tri Dung <dungnt@cbex.xyz>
2018-11-08 16:29:11 +0700

    create todo model

---
+++ src/models/todo.model.ts
@@ -0,0 +1,26 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'boolean',
+  })
+  desc?: boolean;
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
