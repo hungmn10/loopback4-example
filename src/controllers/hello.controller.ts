b45e9b32b6f8e7330e82142d4f786b0faccad399
Nguyen Tri Dung <dungnt@cbex.xyz>
2018-11-08 16:28:50 +0700

    create hello api

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -3,6 +3,11 @@
 // import {inject} from '@loopback/context';
 
 
+import {get} from '@loopback/rest';
+
 export class HelloController {
-  constructor() {}
+  @get('hello')
+  hello(): string {
+    return 'Hello world';
+  }
 }
